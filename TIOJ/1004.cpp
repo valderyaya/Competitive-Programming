1c8f8118ac59aea106ccb65d2bbce9a1487df7fa
valderyaya <41421967+valderyaya@users.noreply.github.com>
2021-04-06 15:58:01 +0800

    Create 1004.cpp

---
+++ TIOJ/1004.cpp
@@ -0,0 +1,23 @@
+//#pragma GCC optimize("Ofast")
+#include<bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+#define REP(i,n) for (int i=0;i<(n);i++)
+#define REP1(i,a,b) for(int i=(a);i<=(b);i++)
+#define em emplace_back
+#define ALL(x) (x).begin(),(x).end()
+#define F first
+#define S second
+#define pii pair<int,int>
+#define mkp make_pair
+#define Rosario ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+//#define lb(x) (x&-x)
+
+int a,b=1;
+int main(){//Rosario
+    cin>>a;
+    while(a>=b+b) b<<=1;
+    cout<<(a-b)*2+1;
+//    system("pause");
+    return 0;
+}
