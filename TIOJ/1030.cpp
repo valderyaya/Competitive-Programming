4d48dd1c6779f2cb3c5182dbc2aa4ff7c73f3e43
valderyaya <41421967+valderyaya@users.noreply.github.com>
2021-04-06 16:12:15 +0800

    Create 1030.cpp

---
+++ TIOJ/1030.cpp
@@ -0,0 +1,29 @@
+//#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#include<bits/stdc++.h>
+//#include<bits/extc++.h>
+//using namespace __gnu_pbds;
+using namespace std;
+#define F first
+#define S second
+typedef long long ll;
+#define REP(i,n) for(int i=0;i<(n);++i)
+#define REP1(i,a,b) for(int i=(a);i<=(b);++i)
+#define em emplace_back
+#define mkp make_pair
+#define ALL(x) (x).begin(),(x).end()
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define Rosario ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+//#define lb(x) (x&-x)
+
+int n,a[100005];
+ll sum;
+int main(){Rosario
+    while(cin>>n,n){sum=1;
+		REP(i,n) cin>>a[i];
+		sort(a,a+n);
+		REP(i,n) sum+=a[i];
+		cout<<sum+a[n-1]<<"\n";
+	}
+    return 0;
+}
