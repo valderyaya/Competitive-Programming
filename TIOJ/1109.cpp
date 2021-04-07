37c066004b9e2e390ce5890c6ca7ffa5c5d86d58
valderyaya <41421967+valderyaya@users.noreply.github.com>
2021-04-07 21:53:01 +0800

    Create 1109.cpp

---
+++ TIOJ/1109.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+#define REP(i,n) for (int i=0;i<(n);i++)
+#define REP1(i,a,b) for(int i=(a);i<=(b);i++)
+#define pb push_back
+#define ALL(x) (x).begin(),(x).end()
+#define SZ(x) ((int)(x).size())
+#define sz(x) (sizeof(x)/sizeof(*x))
+#define F first
+#define S second
+#define mkp make_pair
+//#define ios::sync_with_stdio(false),cin.tie(0);
+
+
+int main(){
+    int n,m;
+    while(cin>>n>>m){
+        string s;
+        REP(i,n){
+            cin>>s;
+            if(i==n-m){
+                cout<<s<<endl;
+            }
+        }
+    }
+//    system("pause");
+    return 0;
+}
