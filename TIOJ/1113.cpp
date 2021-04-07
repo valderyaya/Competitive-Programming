9265db1423476ba91452481e75633ecf69619bce
valderyaya <41421967+valderyaya@users.noreply.github.com>
2021-04-07 21:57:29 +0800

    Create 1113.cpp

---
+++ TIOJ/1113.cpp
@@ -0,0 +1,29 @@
+//#pragma GCC optimize("Ofast")
+#include<bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+#define REP(i,n) for (int i=0;i<(n);i++)
+#define REP1(i,a,b) for(int i=(a);i<=(b);i++)
+#define em emplace_back
+#define ALL(x) (x).begin(),(x).end()
+#define F first
+#define S second
+#define mkp make_pair
+#define pii pair<int,int>
+#define Rosario ios::sync_with_stdio(0),cin.tie(0);
+#define pll pair<ll,ll> 
+//#define lb(x) (x&-x)
+
+string s;
+int main(){
+    Rosario
+    while(cin>>s){
+        sort(ALL(s));
+        do{
+            cout<<s<<"\n";
+        }while(next_permutation(ALL(s)));
+        cout<<"\n";
+    }
+//    system("pause");
+    return 0;
+}
