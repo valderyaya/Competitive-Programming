87ee860e7faaac5bd643c4ecc3f0c6f5643ed8d2
valderyaya <41421967+valderyaya@users.noreply.github.com>
2021-04-09 12:18:27 +0800

    Create 1158.cpp

---
+++ TIOJ/1158.cpp
@@ -0,0 +1,25 @@
+//#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#include<bits/stdc++.h>
+//#include<bits/extc++.h>
+//using namespace __gnu_pbds;
+using namespace std;
+#define F first
+#define S second
+typedef long long ll;
+#define REP(i,n) for(register int i=0;i<(n);++i)
+#define REP1(i,a,b) for(register int i=(a);i<=(b);++i)
+#define em emplace_back
+#define mkp make_pair
+#define pb push_back
+#define ALL(x) (x).begin(),(x).end()
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define Rosario ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+//#define lb(x) (x&-x)
+
+ll n,t;
+int main(){Rosario
+	cin>>t;
+	while(t--) cin>>n,cout<<2*n*n-n+1<<"\n";
+	return 0;
+}
