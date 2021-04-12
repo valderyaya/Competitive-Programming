fd8f1b5accc1eba7ecc5fa54b3c366f75c21d18f
valderyaya <41421967+valderyaya@users.noreply.github.com>
2021-04-12 21:31:22 +0800

    Create 1191.cpp

---
+++ TIOJ/1191.cpp
@@ -0,0 +1,31 @@
+//#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#include<bits/stdc++.h>
+//#include<bits/extc++.h>
+//using namespace __gnu_pbds;
+using namespace std;
+#define F first
+#define S second
+typedef long long ll;
+#define REP(i,n) for(int i=0;i<(n);++i)
+#define REP1(i,a,b) for(int i=(a);i<=(b);++i)
+#define em emplace_back
+#define mkp make_pair
+#define pb push_back
+#define ALL(x) (x).begin(),(x).end()
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define Rosario ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+//#define lb(x) (x&-x)
+
+
+string s;
+int main(){Rosario
+    while(cin>>s,s!="0"){
+        ll x=0;
+        for(int i=0;i<s.size();++i){
+            if(s[i]>'6') --s[i];
+            x=x*9+s[i]-'0';
+        }cout<<x<<'\n';
+    }
+    return 0;
+}
