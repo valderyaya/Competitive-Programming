058c8600ac5377634ad03115ab33f3473e3cccf8
valderyaya <41421967+valderyaya@users.noreply.github.com>
2021-04-13 15:48:16 +0800

    Create 1217.cpp

---
+++ TIOJ/1217.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+#define REP(i,n) for (int i=0;i<(n);i++)
+#define REP1(i,a,b) for(int i=(a);i<=(b);i++)
+#define em emplace_back
+#define ALL(x) (x).begin(),(x).end()
+#define F first
+#define S second
+#define mkp make_pair
+#define pii pair<int,int>
+#define oi ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#define pll pair<ll,ll>
+#define lb(x) (x&-x)
+
+
+int main(){
+    //oi;
+    string s;
+    while(getline(cin,s)) cout<<s<<endl;
+//    system("pause");
+    return 0;
+}
