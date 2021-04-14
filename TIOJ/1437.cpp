f8e4bab73e6a3a514a27f72d6821a1332593c64d
valderyaya <41421967+valderyaya@users.noreply.github.com>
2021-04-14 11:55:50 +0800

    Create 1437.cpp

---
+++ TIOJ/1437.cpp
@@ -0,0 +1,29 @@
+//#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#include<bits/stdc++.h>
+//#include<bits/extc++.h>
+//using namespace __gnu_pbds;
+using namespace std;
+typedef long long ll;
+#define F first
+#define S second
+#define REP(i,n) for(int i=0;i<(n);++i)
+#define REP1(i,a,b) for(int i=(a);i<=(b);++i)
+#define em emplace_back
+#define ALL(x) (x).begin(),(x).end()
+#define mkp make_pair
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define Rosario ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+//#define lb(x) (x&-x)
+
+int n,p=0,mx,x;
+int main(){Rosario
+    cin>>n;
+    REP(i,n){mx=0;
+        REP(j,n){cin>>x;
+            if(x>p) cout<<'('<<j<<','<<i<<")\n";
+            mx=max(mx,x);
+        }p=mx;
+    }
+    return 0;
+}
