954fb7357ae7db32771816ea568df3fca2e84fd4
valderyaya <41421967+valderyaya@users.noreply.github.com>
2021-04-14 11:33:17 +0800

    Create 1367.cpp

---
+++ TIOJ/1367.cpp
@@ -0,0 +1,29 @@
+//#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#include<bits/stdc++.h>
+//#include<bits/extc++.h>
+//using namespace __gnu_pbds;
+using namespace std;
+typedef long long ll;
+#define F first
+#define S second
+#define REP(i,n) for(int i=0;i<(n);++i)
+#define REP1(i,a,b) for(int i=(a);i<=(b);++i)
+#define em emplace_back
+#define ALL(x) (x).begin(),(x).end()
+#define mkp make_pair
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define Rosario ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+//#define lb(x) (x&-x)
+
+int n,a[50005],b[50005];
+int main(){Rosario
+    while(cin>>n){
+    REP(i,n) cin>>a[i];
+    REP(i,n) cin>>b[i];
+    sort(a,a+n); sort(b,b+n,greater<int>());
+    ll s=0;
+    REP(i,n) s+=a[i]*b[i];
+    cout<<s<<"\n";}
+    return 0;
+}
