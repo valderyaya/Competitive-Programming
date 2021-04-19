2e9c9c3554fee5c359bf3a083635f75133184f9a
valderyaya <41421967+valderyaya@users.noreply.github.com>
2021-04-19 14:10:24 +0800

    Create 1586.cpp

---
+++ TIOJ/1586.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+// #include<ext/rope>
+// using namespace __gnu_cxx;
+using namespace std;
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP1(i,a,b) for(int i=(a);i<=(b);i++)
+#define pii pair<int,int>
+typedef long long ll;
+#define mkp make_pair
+#define em emplace_back
+#define F first
+#define S second
+#define ALL(x) (x).begin(),(x).end()
+#define Rosario ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#define pll pair<ll,ll>
+// #define lb(x) (x&-x)
+
+int n;
+string s;
+int main(){Rosario
+    cin>>n;
+    while(n--) cin>>s,cout<<s<<", happy new year!!!\n";
+//    system("pause");
+    return 0;
+}
