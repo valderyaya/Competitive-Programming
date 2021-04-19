93dab70b0f1ef72b2838c4dbd61ed6b92a6b8499
valderyaya <41421967+valderyaya@users.noreply.github.com>
2021-04-19 14:17:04 +0800

    Create 1609.cpp

---
+++ TIOJ/1609.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+#define REP(i,n) for (int i=0;i<(n);i++)
+#define REP1(i,a,b) for(int i=(a);i<=(b);i++)
+#define em emplace_back
+#define ALL(x) (x).begin(),(x).end()
+#define F first
+#define S second
+#define mkp make_pair
+#define pii pair<int,int>
+#define oi ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#define pll pair<ll,ll>
+#define lb(x) (x&-x)
+
+int a[1000005],n;
+int main(){
+    //oi;
+    cin>>n;
+    REP(i,n) cin>>a[i];
+    sort(a,a+n);
+    REP(i,n) cout<<a[i]<<' ';
+//    system("pause");
+    return 0;
+}
