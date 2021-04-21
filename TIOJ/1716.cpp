755bd8b2efc50a8d093d59cd9b18ebd13f10d9b6
valderyaya <41421967+valderyaya@users.noreply.github.com>
2021-04-21 16:37:36 +0800

    Create 1716.cpp

---
+++ TIOJ/1716.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+#define REP(i,n) for (int i=0;i<(n);i++)
+#define REP1(i,a,b) for(int i=(a);i<=(b);i++)
+#define em emplace_back
+#define ALL(x) (x).begin(),(x).end()
+#define F first
+#define S second
+#define pii pair<int,int>
+#define mkp make_pair
+#define Rosario ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+//#define lb(x) (x&(-x)) 
+
+int n,a,b,c;
+int main(){
+    Rosario
+    cin>>n;
+    while(n--){
+        cin>>a>>b>>c;
+        a=b*b-4*a*c;
+        if(a>=0&&sqrt(a)==(int)sqrt(a)) cout<<"Yes\n";
+        else cout<<"No\n";
+    }
+//    system("pause");
+    return 0;
+}
