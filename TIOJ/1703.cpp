9d32036637e3780bb44ec7033a6268cbe1ff7948
valderyaya <41421967+valderyaya@users.noreply.github.com>
2021-04-21 16:35:49 +0800

    Create 1703.cpp

---
+++ TIOJ/1703.cpp
@@ -0,0 +1,26 @@
+//#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#include<bits/stdc++.h>
+//#include<bits/extc++.h>
+//using namespace __gnu_pbds;
+using namespace std;
+#define F first
+#define S second
+typedef long long ll;
+#define REP(i,n) for(int i=0;i<(n);++i)
+#define REP1(i,a,b) for(int i=(a);i<=(b);++i)
+#define em emplace_back
+#define mkp make_pair
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define Rosario ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+//#define lb(x) (x&-x)
+
+
+int n,cnt,x;
+int main(){Rosario
+    while(cin>>n){
+        for(x=3,cnt=0;n>=x;x*=3) cnt+=n/x;
+        cout<<cnt<<"\n";
+    }
+    return 0;
+}
