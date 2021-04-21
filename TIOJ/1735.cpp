f0fa0f4e6f326cdda237fd32a73cff2b6620b6e2
valderyaya <41421967+valderyaya@users.noreply.github.com>
2021-04-21 16:40:05 +0800

    Create 1735.cpp

---
+++ TIOJ/1735.cpp
@@ -0,0 +1,32 @@
+//#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#include<bits/stdc++.h>
+//#include<bits/extc++.h>
+//using namespace __gnu_pbds;
+using namespace std;
+typedef long long ll;
+#define F first
+#define S second
+#define REP(i,n) for(int i=0;i<(n);++i)
+#define REP1(i,a,b) for(int i=(a);i<=(b);++i)
+#define em emplace_back
+#define ALL(x) (x).begin(),(x).end()
+#define mkp make_pair
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define Rosario ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+//#define lb(x) (x&-x)
+
+
+string s;
+int k,n,cnt;
+int main(){Rosario
+    while(cin>>k){
+        cin>>s; n=cnt=0;
+        for(int i=0;i+k<s.size();++i)
+            if(s[i]==s[i+k]){
+                if(++n>=k) ++cnt;
+            }else n=0;
+        cout<<cnt<<"\n";
+    }
+    return 0;
+}
