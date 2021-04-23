ce54d650392d15f9627f6c38de9c7f68dee647bd
valderyaya <41421967+valderyaya@users.noreply.github.com>
2021-04-23 15:34:25 +0800

    Create 2103.cpp

---
+++ TIOJ/2103.cpp
@@ -0,0 +1,29 @@
+//#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#include<bits/stdc++.h>
+//#include<bits/extc++.h>
+//using namespace __gnu_pbds;
+using namespace std;
+#define F first
+#define S second
+typedef long long ll;
+#define REP(i,n) for(int i=0;i<(n);++i)
+#define REP1(i,a,b) for(int i=(a);i<=(b);++i)
+#define em emplace_back
+#define mkp make_pair
+#define pb push_back
+#define ALL(x) (x).begin(),(x).end()
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define Rosario ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+//#define lb(x) (x&-x)
+
+
+int main(){Rosario
+    cout<<"5\n"
+        <<"1 2 9 10 25\n"
+        <<"4 3 8 11 24\n"
+        <<"5 6 7 12 23\n"
+        <<"16 15 14 13 22\n"
+        <<"17 18 19 20 21\n";
+    return 0;
+}
