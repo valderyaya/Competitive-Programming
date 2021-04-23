5a34fe23d091f5a32874926d9ecd603de1c445fc
valderyaya <41421967+valderyaya@users.noreply.github.com>
2021-04-23 17:38:08 +0800

    Create 2169.cpp

---
+++ TIOJ/2169.cpp
@@ -0,0 +1,35 @@
+//#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#include<bits/stdc++.h>
+//#include<bits/extc++.h>
+//using namespace __gnu_pbds;
+using namespace std;
+typedef long long ll;
+#define F first
+#define S second
+#define REP(i,n) for(int i=0;i<(n);++i)
+#define REP1(i,a,b) for(int i=(a);i<=(b);++i)
+#define em emplace_back
+#define ALL(x) (x).begin(),(x).end()
+#define mkp make_pair
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define Rosario ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+//#define lb(x) (x&-x)
+
+int kth(int n, int k) {
+  int res = 0;
+  for (int i = 1; i <= n; ++i) res = (res + k) % i;
+  return res;
+}
+int n,k;
+int main(){Rosario
+    cin>>n>>k;--k;
+    bool c=1;
+    for(int i=2;i<=30000;++i){
+        if(kth(n,i)==k){
+            c=0;cout<<i<<"\n";break;
+        }
+    }
+    if(c) cout<<0;
+    return 0;
+}
