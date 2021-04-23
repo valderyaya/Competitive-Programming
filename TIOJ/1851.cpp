ff14bd7216120963014a2a9c6c748cb4bc8e2ee8
valderyaya <41421967+valderyaya@users.noreply.github.com>
2021-04-23 14:30:31 +0800

    Create 1851.cpp

---
+++ TIOJ/1851.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+#define REP(i,n) for (int i=0;i<(n);i++)
+#define REP1(i,a,b) for(int i=(a);i<=(b);i++)
+#define em emplace_back
+#define ALL(x) (x).begin(),(x).end()
+#define F first
+#define S second
+#define pii pair<int,int>
+#define mkp make_pair
+#define Rosario ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+//#define lb(x) (x&(-x)) 
+
+ll n;
+int main(){//Rosario
+    while(scanf("%lld",&n),n){
+        if(sqrt(n)==(int)sqrt(n)) puts("yes");
+        else puts("no");
+    }
+//    system("pause");
+    return 0;
+}
