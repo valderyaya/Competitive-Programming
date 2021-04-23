4ce0fb9c049ca6144ba86b78cfc94a83516bf2dc
valderyaya <41421967+valderyaya@users.noreply.github.com>
2021-04-23 15:26:05 +0800

    Create 2020.cpp

---
+++ TIOJ/2020.cpp
@@ -0,0 +1,34 @@
+//#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#include<bits/stdc++.h>
+//#include<bits/extc++.h>
+//using namespace __gnu_pbds;
+using namespace std;
+#define F first
+#define S second
+typedef long long ll;
+#define REP(i,n) for(int i=0;i<(n);++i)
+#define REP1(i,a,b) for(int i=(a);i<=(b);++i)
+#define em emplace_back
+#define mkp make_pair
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define Rosario ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+//#define lb(x) (x&-x)
+
+int dp[105][105];
+int dfs(int x,int y){
+    if(x<0||y<0) return 1e9;
+    if(x>y) x^=y^=x^=y;
+    if(x==y) return 1;
+    if(dp[x][y]) return dp[x][y];
+    int res=1e9;
+    REP1(i,1,x-1) res=min(res,dfs(i,y)+dfs(x-i,y));
+    REP1(i,1,y-1) res=min(res,dfs(x,y-i)+dfs(x,i));
+    return dp[x][y]=res;
+}
+int main(){Rosario
+    int n,m;
+    cin>>n>>m;
+    cout<<dfs(n,m)<<"\n";
+    return 0;
+}
