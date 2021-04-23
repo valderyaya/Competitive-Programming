f8856e2eba54f4d4416f96848c8ef8d0e63f6a8e
valderyaya <41421967+valderyaya@users.noreply.github.com>
2021-04-23 17:34:46 +0800

    Create 2138.cpp

---
+++ TIOJ/2138.cpp
@@ -0,0 +1,25 @@
+//#pragma GCC optimize("Ofast")
+#include<bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+#define REP(i,n) for (int i=0;i<(n);i++)
+#define REP1(i,a,b) for(int i=(a);i<=(b);i++)
+#define em emplace_back
+#define ALL(x) (x).begin(),(x).end()
+#define F first
+#define S second
+#define mkp make_pair
+#define pii pair<int,int>
+#define Rosario ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#define pll pair<ll,ll>
+//#define lb(x) (x&-x)
+
+string s="0.0000\n";
+int a,b,c,d,e,f;
+int main(){//Rosario
+    cin>>a>>b>>c>>d>>e>>f;
+    while(f--) cin>>a>>b>>c;
+    cout<<s;
+//    system("pause");
+    return 0;
+}
