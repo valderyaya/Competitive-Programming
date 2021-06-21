fa84ebc6e037e111f99bf259fa36dd2f0e58f9ba
valderyaya <41421967+valderyaya@users.noreply.github.com>
2021-06-22 00:20:12 +0800

    Create Prefix_Matches.cpp

---
+++ CS_Academy/Prefix_Matches.cpp
@@ -0,0 +1,31 @@
+//#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#include<bits/stdc++.h>
+//#include<bits/extc++.h>
+//using namespace __gnu_pbds;
+using namespace std;
+#define F first
+#define S second
+typedef long long ll;
+#define REP(i,n) for(register int i=0;i<(n);++i)
+#define REP1(i,a,b) for(register int i=(a);i<=(b);++i)
+#define em emplace_back
+#define mkp make_pair
+#define pb push_back
+#define ALL(x) (x).begin(),(x).end()
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define Rosario ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#define lb(x) (x&-x)
+
+
+int n,f[100005],x;
+signed main(){Rosario
+    cin>>n; --n;
+    for(int i=1,j=1;i<=n;j+=(i++==j)){
+        cin>>x;
+        if(x&&j<=i+x-1)
+            for(int k=j-i+1;k<=x;++k) f[j++]=k;
+        cout<<f[i]<<' ';
+    }
+    return 0;
+}
