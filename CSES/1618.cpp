63855c36933ca14e468f3373167c8badfc0295ef
valderyaya <41421967+valderyaya@users.noreply.github.com>
2022-05-12 17:21:26 +0800

    Create 1618.cpp

---
+++ CSES/1618.cpp
@@ -0,0 +1,26 @@
+//#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#include<bits/stdc++.h>
+//#include<bits/extc++.h>
+//using namespace __gnu_pbds;
+using namespace std;
+#define F first
+#define S second
+typedef long long ll;
+#define REP(i,n) for(register int i=0;i<(n);++i)
+#define REP1(i,a,b) for(register int i=(a);i<=(b);++i)
+#define em emplace_back
+#define mkp make_pair
+#define pb push_back
+#define ALL(x) (x).begin(),(x).end()
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define Aincrad ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#define lb(x) (x&-x)
+ 
+ll n,cnt;
+signed main(){Aincrad
+    cin>>n;
+    for(ll x=5;n>=x;x*=5) cnt+=n/x;
+    cout<<cnt<<"\n";
+    return 0;
+}
