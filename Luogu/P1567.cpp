017438e40229d0214d7bfdafac7698ab6b87c2c5
valderyaya <41421967+valderyaya@users.noreply.github.com>
2022-06-18 16:19:57 +0800

    Create P1567.cpp

---
+++ Luogu/P1567.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define REP(i,n) for (int i=0;i<(n);i++)
+#define REP1(i,a,b) for(int i=(a);i<=(b);i++)
+//#define pb push_back
+#define em emplace_back
+#define ALL(x) (x).begin(),(x).end()
+#define F first
+#define S second
+#define mkp make_pair
+
+
+int main(){
+    //ios::sync_with_stdio(false),cin.tie(0),cout.tie(0);
+    int n,m,cnt=0,ma=0,c=0;
+    cin>>n;
+    REP(i,n){
+        cin>>m;
+        if(m>ma){cnt++,ma=m;}
+        else{
+            c=max(c,cnt);
+            cnt=1;ma=m;
+        }
+    }
+    cout<<c;
+ //   system("pause");
+    return 0;
+}
