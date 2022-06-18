0c456fb0dd952530795341e2ec5c2df73a96967f
valderyaya <41421967+valderyaya@users.noreply.github.com>
2022-06-18 11:33:14 +0800

    Create P2320.cpp

---
+++ Luogu/P2320.cpp
@@ -0,0 +1,28 @@
+//#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#include<bits/stdc++.h>
+//#include<bits/extc++.h>
+//using namespace __gnu_pbds;
+using namespace std;
+#define F first
+#define S second
+typedef long long ll;
+#define REP(i,n) for(register int i=0;i<(n);++i)
+#define REP1(i,a,b) for(register int i=(a);i<=(b);++i)
+#define em emplace_back
+#define mkp make_pair
+#define pb push_back
+#define ALL(x) (x).begin(),(x).end()
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define Aincrad ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#define lb(x) (x&-x)
+ 
+int n,i;
+vector<int> v;
+signed main(){Aincrad
+	cin>>n;
+	for(;n;v.em(n+1>>1),n>>=1);
+	cout<<v.size()<<"\n";
+	for(int i=v.size()-1;~i;--i) cout<<v[i]<<' ';
+	return 0;
+}
