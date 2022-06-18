a763143fdb03d8147c7ccdd227001effeab56683
valderyaya <41421967+valderyaya@users.noreply.github.com>
2022-06-18 11:26:10 +0800

    Create 952A.cpp

---
+++ Codeforces/952A.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+#define REP(i,n) for (int i=0;i<(n);i++)
+
+int main(){
+    int a;
+    cin>>a;a&=1;
+    cout<<a;
+ //   system("pause");
+    return 0;
+}
