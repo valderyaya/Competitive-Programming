e7ec2c027efca35dff95a524e030ec2ee0d3d42b
valderyaya <41421967+valderyaya@users.noreply.github.com>
2022-06-18 14:55:23 +0800

    Create P5718.cpp

---
+++ Luogu/P5718.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+//#include<bits/extc++.h>
+//using namespace __gnu_pbds;
+using namespace std;
+typedef long long ll;
+#define F first
+#define S second
+#define REP(i,n) for(int i=0;i<(n);++i)
+#define REP1(i,a,b) for(int i=(a);i<=(b);++i)
+#define em emplace_back
+#define ALL(x) (x).begin(),(x).end()
+#define mkp make_pair
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define Rosario ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+//#define lb(x) (x&-x)
+#define wait system("pause");
+
+
+int a[101],n;
+int main(){Rosario
+    cin>>n;
+    REP(i,n) cin>>a[i];
+    cout<<*min_element(a,a+n)<<"\n";
+//    system("pause");
+    return 0;
+}
