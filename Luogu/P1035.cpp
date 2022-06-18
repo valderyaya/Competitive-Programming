95c001e8b4be0f844858354093366ce3909e5274
valderyaya <41421967+valderyaya@users.noreply.github.com>
2022-06-18 16:21:51 +0800

    Create P1035.cpp

---
+++ Luogu/P1035.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define REP(i,n) for (int i=0;i<(n);i++)
+#define REP1(i,a,b) for(int i=(a);i<=(b);i++)
+//#define pb push_back
+#define em emplace_back
+#define ALL(x) (x).begin(),(x).end()
+#define F first
+#define S second
+#define mkp make_pair
+
+
+int main(){
+    //ios::sync_with_stdio(false),cin.tie(0),cout.tie(0);
+    double n,m=1,k=2;
+    cin>>n;
+    while(n>=m){
+        m+=1/k++;
+    }
+    cout<<k-1;
+//    system("pause");
+    return 0;
+}
