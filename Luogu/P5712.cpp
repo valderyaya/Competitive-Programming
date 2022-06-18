45584c5bee9c86ad47321d91c89501b6d5f171a2
valderyaya <41421967+valderyaya@users.noreply.github.com>
2022-06-18 15:41:25 +0800

    Create P5712.cpp

---
+++ Luogu/P5712.cpp
@@ -0,0 +1,24 @@
+//#pragma GCC optimize("Ofast")
+#include<bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+#define REP(i,n) for (int i=0;i<(n);i++)
+#define REP1(i,a,b) for(int i=(a);i<=(b);i++)
+#define em emplace_back
+#define ALL(x) (x).begin(),(x).end()
+#define F first
+#define S second
+#define mkp make_pair
+#define pii pair<int,int>
+#define Rosario ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#define pll pair<ll,ll> 
+//#define lb(x) (x&-x)
+
+int n;
+int main(){//Rosario
+    cin>>n;
+    if(n<2) cout<<"Today, I ate "<<n<<" apple.";
+    else cout<<"Today, I ate "<<n<<" apples.";
+//    system("pause");
+    return 0;
+}
