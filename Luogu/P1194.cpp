3a60f178463dd83e17982db371caa4432ea83781
valderyaya <41421967+valderyaya@users.noreply.github.com>
2022-06-18 14:13:14 +0800

    Create P1194.cpp

---
+++ Luogu/P1194.cpp
@@ -0,0 +1,41 @@
+//#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#include<bits/stdc++.h>
+//#include<bits/extc++.h>
+//using namespace __gnu_pbds;
+using namespace std;
+typedef long long ll;
+#define F first
+#define S second
+#define REP(i,n) for(int i=0;i<(n);++i)
+#define REP1(i,a,b) for(int i=(a);i<=(b);++i)
+#define em emplace_back
+#define ALL(x) (x).begin(),(x).end()
+#define mkp make_pair
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define Rosario ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+//#define lb(x) (x&-x)
+
+struct edge{
+    int x,y,c;
+    bool operator<(const edge &a)const{return c<a.c;}
+}e[125050];
+int p[505],idx,n,m,cnt=1,sum;
+int fin(int x){return p[x]==x?x:p[x]=fin(p[x]);}
+int main(){Rosario
+    cin>>m>>n; int x,y,z;
+    REP1(i,1,n){
+        p[i]=i; e[idx++]=edge{i,0,m};
+        REP1(j,1,n){cin>>x;
+            if(!x||j<=i||x>m) continue;
+            e[idx++]=edge{i,j,x};
+        }
+    }sort(e,e+idx);
+    REP(i,idx){
+        x=fin(e[i].x),y=fin(e[i].y);z=e[i].c;
+        if(x!=y) p[x]=y,sum+=z,cnt++;
+        if(cnt==n+1) break;
+    }
+    cout<<sum<<"\n";
+    return 0;
+}
