738539ccff7873a62904bb4b8bcf657306f00a43
valderyaya <41421967+valderyaya@users.noreply.github.com>
2022-06-18 14:49:15 +0800

    Create P1100.cpp

---
+++ Luogu/P1100.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+//#include<bits/extc++.h>
+//using namespace __gnu_pbds;
+using namespace std;
+typedef long long ll;
+#define F first
+#define S second
+#define REP(i,n) for(int i=0;i<(n);++i)
+#define REP1(i,a,b) for(int i=(a);i<=(b);++i)
+#define em emplace_back
+#define ALL(x) (x).begin(),(x).end()
+#define mkp make_pair
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define Rosario ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+//#define lb(x) (x&-x)
+#define wait system("pause");
+
+
+int main(){Rosario
+    ll n;
+    cin>>n;
+    cout<<n/65536+n%65536*65536<<"\n";
+//    system("pause");
+    return 0;
+}
