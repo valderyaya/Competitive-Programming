947d3e1e7a0dbb83376a5893198a02a20ad20b94
valderyaya <41421967+valderyaya@users.noreply.github.com>
2022-06-18 15:41:37 +0800

    Create P5711.cpp

---
+++ Luogu/P5711.cpp
@@ -0,0 +1,24 @@
+//#pragma GCC optimize("Ofast")
+#include<bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+#define REP(i,n) for (int i=0;i<(n);i++)
+#define REP1(i,a,b) for(int i=(a);i<=(b);i++)
+#define em emplace_back
+#define ALL(x) (x).begin(),(x).end()
+#define F first
+#define S second
+#define mkp make_pair
+#define pii pair<int,int>
+#define Rosario ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#define pll pair<ll,ll> 
+//#define lb(x) (x&-x)
+
+int n;
+int main(){//Rosario
+    cin>>n;
+    if((n%400==0)||(n%4==0&&n%100)) cout<<1;
+    else cout<<0;
+//    system("pause");
+    return 0;
+}
