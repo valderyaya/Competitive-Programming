c7fb36478220f5f20e9b0380e049dd0caabec0ad
valderyaya <41421967+valderyaya@users.noreply.github.com>
2022-06-18 15:44:42 +0800

    Create P5706.cpp

---
+++ Luogu/P5706.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+// #include<ext/rope>
+// using namespace __gnu_cxx;
+using namespace std;
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP1(i,a,b) for(int i=(a);i<=(b);i++)
+#define pii pair<int,int>
+typedef long long ll;
+#define mkp make_pair
+#define em emplace_back
+#define F first
+#define S second
+#define ALL(x) (x).begin(),(x).end()
+#define Rosario ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#define pll pair<ll,ll>
+//#define lb(x) (x&-x)
+
+double s;
+int n;
+int main(){//Rosario
+    cin>>s>>n;
+    cout<<fixed<<setprecision(3)<<(double)s/n<<"\n"<<(int)n*2<<"\n";
+//    system("pause");
+    return 0;
+}
