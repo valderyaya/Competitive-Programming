bc1c780ea21d04a18315bc0e122594ed7ed5b6f9
valderyaya <41421967+valderyaya@users.noreply.github.com>
2022-06-18 14:30:02 +0800

    Create P1918.cpp

---
+++ Luogu/P1918.cpp
@@ -0,0 +1,34 @@
+//#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#include<bits/stdc++.h>
+//#include<bits/extc++.h>
+//using namespace __gnu_pbds;
+using namespace std;
+typedef long long ll;
+#define F first
+#define S second
+#define REP(i,n) for(int i=0;i<(n);++i)
+#define REP1(i,a,b) for(int i=(a);i<=(b);++i)
+#define em emplace_back
+#define ALL(x) (x).begin(),(x).end()
+#define mkp make_pair
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define Rosario ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+//#define lb(x) (x&-x)
+#define wait system("pause");
+
+
+int n,m,x;
+map<int,int> mp;
+int main(){Rosario
+    cin>>n;
+    REP1(i,1,n) cin>>x,mp[x]=i;
+    cin>>m;
+    while(m--){
+        cin>>x;
+        if(mp.count(x)) cout<<mp[x]<<"\n";
+        else cout<<0<<"\n";
+    }
+//    system("pause");
+    return 0;
+}
