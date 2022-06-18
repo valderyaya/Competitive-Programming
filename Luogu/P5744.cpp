087c76cd0f4e6eca15fcf0ef94ef6fbba3229962
valderyaya <41421967+valderyaya@users.noreply.github.com>
2022-06-18 15:25:58 +0800

    Create P5744.cpp

---
+++ Luogu/P5744.cpp
@@ -0,0 +1,30 @@
+//#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#include<bits/stdc++.h>
+//#include<bits/extc++.h>
+//using namespace __gnu_pbds;
+using namespace std;
+typedef long long ll;
+#define F first
+#define S second
+#define REP(i,n) for(int i=0;i<(n);++i)
+#define REP1(i,a,b) for(int i=(a);i<=(b);++i)
+#define em emplace_back
+#define ALL(x) (x).begin(),(x).end()
+#define mkp make_pair
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define Rosario ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+//#define lb(x) (x&-x)
+//#define line cout<<"-------------------------\n";
+
+int t,a,b;
+string s;
+int main(){//Rosario
+    cin>>t;
+    while(t--){
+        cin>>s>>a>>b;
+        cout<<s<<' '<<a+1<<' '<<min(600,b/5*6)<<"\n";
+    }
+//    system("pause");
+    return 0;
+}
