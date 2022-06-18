2771d6a92ea1e31e91b0cd946ff4a1b35a3d8ce0
valderyaya <41421967+valderyaya@users.noreply.github.com>
2022-06-18 16:22:32 +0800

    Create P1909.cpp

---
+++ Luogu/P1909.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define REP(i,n) for (int i=0;i<(n);i++)
+#define REP1(i,a,b) for(int i=(a);i<=(b);i++)
+//#define pb push_back
+#define em emplace_back
+#define ALL(x) (x).begin(),(x).end()
+#define F first
+#define S second
+#define mkp make_pair
+
+
+int main(){
+    //ios::sync_with_stdio(false),cin.tie(0),cout.tie(0);
+    int n,a,b,mi=1e9;
+    cin>>n;
+    REP(i,3){
+        cin>>a>>b;
+        mi=min(mi,n/a*b+(n%a?1:0)*b);
+    }
+    cout<<mi;
+//   system("pause");
+    return 0;
+}
