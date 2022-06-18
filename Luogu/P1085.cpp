e1037fb29761a22f70aaa8277d70dea6ac0791c3
valderyaya <41421967+valderyaya@users.noreply.github.com>
2022-06-18 16:22:54 +0800

    Create P1085.cpp

---
+++ Luogu/P1085.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define REP(i,n) for (int i=0;i<(n);i++)
+#define REP1(i,a,b) for(int i=(a);i<=(b);i++)
+//#define pb push_back
+#define em emplace_back
+#define ALL(x) (x).begin(),(x).end()
+#define F first
+#define S second
+#define mkp make_pair
+
+
+int main(){
+    //ios::sync_with_stdio(false),cin.tie(0),cout.tie(0);
+    int a,b,ma=0,ans=0;
+    REP(i,7){
+        cin>>a>>b;
+        if(a+b>8&&a+b>ma){
+            ma=a+b;
+            ans=i+1;
+        }
+    }
+    cout<<ans;
+//   system("pause");
+    return 0;
+}
