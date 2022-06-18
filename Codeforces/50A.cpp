f66b38ad8067a2da8089f8647678c61d2dc1acf3
valderyaya <41421967+valderyaya@users.noreply.github.com>
2022-06-18 11:27:05 +0800

    Create 50A.cpp

---
+++ Codeforces/50A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+#define REP(i,n) for (int i=0;i<(n);i++)
+#define REP1(i,a,b) for(int i=(a);i<=(b);i++)
+#define pb push_back
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    cout<<a*b/2;
+   // system("pause");
+    return 0;
+}
