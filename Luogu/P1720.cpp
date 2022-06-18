614cd5c5e2a82470f5fbf346daf74337c3b6d03a
valderyaya <41421967+valderyaya@users.noreply.github.com>
2022-06-18 15:05:46 +0800

    Create P1720.cpp

---
+++ Luogu/P1720.cpp
@@ -0,0 +1,28 @@
+//#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#include<bits/stdc++.h>
+//#include<bits/extc++.h>
+//using namespace __gnu_pbds;
+using namespace std;
+typedef long long ll;
+#define F first
+#define S second
+#define REP(i,n) for(int i=0;i<(n);++i)
+#define REP1(i,a,b) for(int i=(a);i<=(b);++i)
+#define em emplace_back
+#define ALL(x) (x).begin(),(x).end()
+#define mkp make_pair
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define Rosario ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+//#define lb(x) (x&-x)
+#define wait system("pause");
+
+ll f[50]={0,1,1};
+int main(){//Rosario
+    for(int i=3;i<=48;i++) f[i]=f[i-1]+f[i-2];
+    ll n;
+    cin>>n;
+    cout<<fixed<<setprecision(2)<<(double)f[n]<<"\n";
+//    system("pause");
+    return 0;
+}
