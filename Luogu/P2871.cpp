0bf8b5fe31d6d88e645e4bd5206510a18083ef89
valderyaya <41421967+valderyaya@users.noreply.github.com>
2022-06-18 14:08:56 +0800

    Create P2871.cpp

---
+++ Luogu/P2871.cpp
@@ -0,0 +1,27 @@
+//#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#include<bits/stdc++.h>
+//#include<bits/extc++.h>
+//using namespace __gnu_pbds;
+using namespace std;
+typedef long long ll;
+#define F first
+#define S second
+#define REP(i,n) for(int i=0;i<(n);++i)
+#define REP1(i,a,b) for(int i=(a);i<=(b);++i)
+#define em emplace_back
+#define ALL(x) (x).begin(),(x).end()
+#define mkp make_pair
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define Rosario ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+//#define lb(x) (x&-x)
+
+int dp[13000],n,m,x,y;
+int main(){Rosario
+    cin>>n>>m;
+    while(n--){
+        cin>>x>>y;
+        for(int i=m;i>=x;--i) dp[i]=max(dp[i],dp[i-x]+y);
+    }cout<<dp[m]<<"\n";
+    return 0;
+}
