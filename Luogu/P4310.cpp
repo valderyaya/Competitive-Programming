a0a067b2e9b74c0d9f2c4a8b287ee75f8b978c7b
valderyaya <41421967+valderyaya@users.noreply.github.com>
2022-06-18 12:40:22 +0800

    Create P4310.cpp

---
+++ Luogu/P4310.cpp
@@ -0,0 +1,32 @@
+//#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#include<bits/stdc++.h>
+//#include<bits/extc++.h>
+//using namespace __gnu_pbds;
+using namespace std;
+#define F first
+#define S second
+typedef long long ll;
+#define REP(i,n) for(register int i=0;i<(n);++i)
+#define REP1(i,a,b) for(register int i=(a);i<=(b);++i)
+#define em emplace_back
+#define mkp make_pair
+#define pb push_back
+#define ALL(x) (x).begin(),(x).end()
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define Rosario ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+//#define lb(x) (x&-x)
+
+int n,f[32],x,mx;
+int main(){Rosario
+	cin>>n;
+	REP1(i,1,n){
+		cin>>x;
+		REP(j,32) if(x>>j&1) mx=max(mx,f[j]+1);
+		REP(j,32) if(x>>j&1) f[j]=mx;
+		mx=0;
+	}
+	REP(i,32) mx=max(mx,f[i]);
+	cout<<mx<<"\n";
+	return 0;
+}
