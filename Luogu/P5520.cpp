323a6456edc3102f3ae4eddd084a633987bd6bd8
valderyaya <41421967+valderyaya@users.noreply.github.com>
2022-06-18 13:10:52 +0800

    Create P5520.cpp

---
+++ Luogu/P5520.cpp
@@ -0,0 +1,26 @@
+//#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#include<bits/stdc++.h>
+//#include<bits/extc++.h>
+//using namespace __gnu_pbds;
+using namespace std;
+#define F first
+#define S second
+typedef long long ll;
+#define REP(i,n) for(register int i=0;i<(n);++i)
+#define REP1(i,a,b) for(register int i=(a);i<=(b);++i)
+#define em emplace_back
+#define mkp make_pair
+#define pb push_back
+#define ALL(x) (x).begin(),(x).end()
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define Rosario ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+//#define lb(x) (x&-x)
+
+ll t,n,m,p,ans=1;
+int main(){Rosario
+	cin>>t>>n>>m>>p;
+	for(ll i=n-m+1;i>=n-m-m+2;--i) ans=(ans*i)%p;
+	cout<<ans<<"\n";
+	return 0;
+}
