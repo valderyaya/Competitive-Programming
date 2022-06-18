aad293d12b4d5feb5200073932a0275d88268c77
valderyaya <41421967+valderyaya@users.noreply.github.com>
2022-06-18 11:26:22 +0800

    Create 4A.cpp

---
+++ Codeforces/4A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+#define REP(i,n) for (int i=0;i<(n);i++)
+#define REP1(i,a,b) for(int i=(a);i<=(b);i++)
+#define pb push_back
+int main(){
+    int a;
+    cin>>a;
+    if(!(a%2)&& a!=2) cout<<"YES";
+    else cout<<"NO";
+  //  system("pause");
+    return 0;
+}
