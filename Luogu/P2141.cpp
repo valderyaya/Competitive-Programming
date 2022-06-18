f7d2c7a6846c4330749a7542a9b510bff39800b1
valderyaya <41421967+valderyaya@users.noreply.github.com>
2022-06-18 16:20:09 +0800

    Create P2141.cpp

---
+++ Luogu/P2141.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define REP(i,n) for (int i=0;i<(n);i++)
+#define REP1(i,a,b) for(int i=(a);i<=(b);i++)
+//#define pb push_back
+#define em emplace_back
+#define ALL(x) (x).begin(),(x).end()
+#define F first
+#define S second
+#define mkp make_pair
+
+
+int main(){
+    //ios::sync_with_stdio(false),cin.tie(0),cout.tie(0);
+    set<int> s,b;
+    int n,a[101];
+    cin>>n;
+    REP(i,n){cin>>a[i];s.insert(a[i]);}
+    REP(i,n){
+        for(int j=i+1;j<n;j++){
+            if(s.find(a[i]+a[j])!=s.end()) b.insert(a[i]+a[j]);
+        }
+    }
+    cout<<b.size();
+//    system("pause");
+    return 0;
+}
+
