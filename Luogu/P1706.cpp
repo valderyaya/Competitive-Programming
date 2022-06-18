a6679319c5bf275fd268bd0438e7b245a526f648
valderyaya <41421967+valderyaya@users.noreply.github.com>
2022-06-18 13:41:06 +0800

    Create P1706.cpp

---
+++ Luogu/P1706.cpp
@@ -0,0 +1,29 @@
+//#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#include<bits/stdc++.h>
+//#include<bits/extc++.h>
+//using namespace __gnu_pbds;
+using namespace std;
+#define F first
+#define S second
+typedef long long ll;
+#define REP(i,n) for(register int i=0;i<(n);++i)
+#define REP1(i,a,b) for(register int i=(a);i<=(b);++i)
+#define em emplace_back
+#define mkp make_pair
+#define pb push_back
+#define ALL(x) (x).begin(),(x).end()
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define Rosario ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+//#define lb(x) (x&-x)
+
+int n,a[10];
+int main(){Rosario
+    cin>>n;
+    REP1(i,1,n) a[i]=i;
+    do{
+        REP1(i,1,n) cout<<setw(5)<<a[i];
+        cout<<"\n";
+    }while(next_permutation(a+1,a+1+n));
+    return 0;
+}
