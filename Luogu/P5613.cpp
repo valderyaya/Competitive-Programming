2b0d0558dcb061f1994a114726f4f84e9c6153e4
valderyaya <41421967+valderyaya@users.noreply.github.com>
2022-06-18 16:00:34 +0800

    Create P5613.cpp

---
+++ Luogu/P5613.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+#define REP(i,n) for (int i=0;i<(n);i++)
+#define REP1(i,a,b) for(int i=(a);i<=(b);i++)
+#define em emplace_back
+#define ALL(x) (x).begin(),(x).end()
+#define F first
+#define S second
+#define pii pair<int,int>
+#define mkp make_pair
+#define Rosario ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+//#define lb(x) (x&(-x)) 
+
+queue<int> q;
+int n,a,b,c;
+int main(){
+    //Rosario
+    cin>>a>>b>>c;
+    while(c--) cin>>n,q.emplace(n);
+    for(c=n=0;n<a;n+=b,c++){
+        while(q.size()&&q.front()<n) q.pop();
+        if(q.size()&&n==q.front()) b++,q.pop();
+    }
+    cout<<c<<"\n";
+//    system("pause");
+    return 0;
+}
