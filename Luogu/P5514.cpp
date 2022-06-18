7890aa2519fa8f770e32cf4675790d71736cd366
valderyaya <41421967+valderyaya@users.noreply.github.com>
2022-06-18 13:56:30 +0800

    Create P5514.cpp

---
+++ Luogu/P5514.cpp
@@ -0,0 +1,24 @@
+//#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#include<bits/stdc++.h>
+//#include<bits/extc++.h>
+//using namespace __gnu_pbds;
+using namespace std;
+typedef long long ll;
+#define F first
+#define S second
+#define REP(i,n) for(int i=0;i<(n);++i)
+#define REP1(i,a,b) for(int i=(a);i<=(b);++i)
+#define em emplace_back
+#define ALL(x) (x).begin(),(x).end()
+#define mkp make_pair
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define Rosario ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+//#define lb(x) (x&-x)
+
+int n,m,x;
+int main(){Rosario
+    for(cin>>n;n;n--) cin>>x,m^=x;
+    cout<<m<<"\n";
+    return 0;
+}
