a529e5ab6d2baca86a85a6648dcbf6c810c1612b
valderyaya <41421967+valderyaya@users.noreply.github.com>
2022-06-18 16:02:46 +0800

    Create P3367.cpp

---
+++ Luogu/P3367.cpp
@@ -0,0 +1,35 @@
+//#pragma GCC optimize("Ofast")
+#include<bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+#define REP(i,n) for (int i=0;i<(n);i++)
+#define REP1(i,a,b) for(int i=(a);i<=(b);i++)
+#define em emplace_back
+#define ALL(x) (x).begin(),(x).end()
+#define F first
+#define S second
+#define mkp make_pair
+#define pii pair<int,int>
+#define oi ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#define pll pair<ll,ll> 
+//#define lb(x) (x&-x)
+
+int n,m,pa[10005],ran[10005]={};
+int fin(int x){return pa[x]==x?x:pa[x]=fin(pa[x]);}
+int main(){
+    oi;
+    cin>>n>>m;
+    REP1(i,1,n) pa[i]=i;
+    int a,b,c;
+    REP(i,m){
+        cin>>a>>b>>c;
+        if(a&1){
+            int x=fin(b),y=fin(c);
+            if(ran[x]>ran[y]) pa[y]=x;
+            else if(ran[x]<ran[y]) pa[x]=y;
+            else pa[x]=y,ran[x]++;
+        }else cout<<(fin(b)==fin(c)?"Y\n":"N\n");
+    }
+//    system("pause");
+    return 0;
+}
