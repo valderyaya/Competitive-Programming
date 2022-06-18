d255939d1d82e3f421674cdf1a184e237f4f4b57
valderyaya <41421967+valderyaya@users.noreply.github.com>
2022-06-18 15:07:02 +0800

    Create P1765.cpp

---
+++ Luogu/P1765.cpp
@@ -0,0 +1,30 @@
+//#pragma GCC optimize("Ofast")
+#include<bits/stdc++.h>
+//#include<bits/extc++.h>
+//using namespace __gnu_pbds;
+typedef long long ll;
+using namespace std;
+#define REP(i,n) for (int i=0;i<(n);i++)
+#define REP1(i,a,b) for(int i=(a);i<=(b);i++)
+#define em emplace_back
+#define ALL(x) (x).begin(),(x).end()
+#define F first
+#define S second
+#define mkp make_pair
+#define pii pair<int,int>
+#define Rosario ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#define pll pair<ll,ll> 
+//#define lb(x) (x&-x)
+//#define line cout<<"-------------------------------\n";
+
+string s;
+int sum=0,m[]={1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,4,1,2,3,1,2,3,4};
+int main(){//Rosario
+    getline(cin,s);
+    for(auto &c:s){
+        if(c==' ') sum++;
+        else if(isalpha(c)) sum+=m[c-'a'];
+    }cout<<sum<<"\n";
+//    system("pause");
+    return 0;
+}
