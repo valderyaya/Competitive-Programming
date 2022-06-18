2ab6c3e4ab948aacbf22456150aec0616fdef90c
valderyaya <41421967+valderyaya@users.noreply.github.com>
2022-06-18 11:18:00 +0800

    Create abc232-pa.cpp

---
+++ Atcoder/abc232/abc232-pa.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define REP(i,n) for(int i=0;i<n;++i)
+#define REP1(i,a,b) for(int i=a;i<=b;++i)
+#define all(x) x.begin(),x.end()
+#define em emplace_back
+#define Rosario ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+
+signed main(){Rosario
+	int x,y;
+	string s;
+	cin>>s;
+	cout<<(s[0]-'0')*(s[2]-'0')<<"\n";	
+}
