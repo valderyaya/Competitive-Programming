0b7c0f51beb6dfbc0f47861a5d27a55611a6d3ae
valderyaya <41421967+valderyaya@users.noreply.github.com>
2022-06-18 15:58:49 +0800

    Create P1060.cpp

---
+++ Luogu/P1060.cpp
@@ -0,0 +1,26 @@
+//#pragma GCC optimize("Ofast")
+#include<bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+#define REP(i,n) for (int i=0;i<(n);i++)
+#define REP1(i,a,b) for(int i=(a);i<=(b);i++)
+#define em emplace_back
+#define ALL(x) (x).begin(),(x).end()
+#define F first
+#define S second
+#define pii pair<int,int>
+#define mkp make_pair
+#define Rosario ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+//#define lb(x) (x&(-x)) 
+
+int n,m,dp[30005];
+pii a[25];
+int main(){Rosario
+    cin>>n>>m;
+    REP(i,m) cin>>a[i].F>>a[i].S;
+    REP(i,m)
+        for(int j=n;j>=a[i].F;j--) dp[j]=max(dp[j],dp[j-a[i].F]+a[i].S*a[i].F);
+    cout<<dp[n]<<"\n";
+//    system("pause");
+    return 0;
+}
