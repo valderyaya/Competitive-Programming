d52c1d7dca08e5e9ff294a73d0e9788bbe74bb56
valderyaya <41421967+valderyaya@users.noreply.github.com>
2022-06-18 16:21:36 +0800

    Create P1423.cpp

---
+++ Luogu/P1423.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define REP(i,n) for (int i=0;i<(n);i++)
+#define REP1(i,a,b) for(int i=(a);i<=(b);i++)
+//#define pb push_back
+#define em emplace_back
+#define ALL(x) (x).begin(),(x).end()
+#define F first
+#define S second
+#define mkp make_pair
+
+
+int main(){
+    //ios::sync_with_stdio(false),cin.tie(0),cout.tie(0);
+    double n,i=2,m=0;
+    int cnt=0;
+    cin>>n;
+    while(m<n){
+        m+=i;
+        i*=0.98;
+        cnt++;
+    }
+    cout<<cnt;
+//    system("pause");
+    return 0;
+}
