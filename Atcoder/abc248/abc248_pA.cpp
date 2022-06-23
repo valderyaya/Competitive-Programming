79897e407722fdf785f3504727c94646943ab13e
valderyaya <41421967+valderyaya@users.noreply.github.com>
2022-06-23 11:22:34 +0800

    Create abc248_pA.cpp

---
+++ Atcoder/abc248/abc248_pA.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define valder ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#define ll long long
+#define F first
+#define S second
+#define ALL(x) x.begin(),x.end()
+#define pii pair<int,int>
+#define REP(i,n) for(int i=0;i<n;++i)
+#define REP1(i,a,b) for(int i=a;i<=b;++i)
+#define em emplace_back
+#define lb(x) x&-x
+#define pll pair<ll,ll>
+#define mkp make_pair
+#define endl "\n"
+
+int n=45;
+string s;
+signed main(){valder
+	cin>>s;
+	for(auto &i:s) n-=i-'0';
+	cout<<n;
+	return 0;
+}
