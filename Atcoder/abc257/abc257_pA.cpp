a925198a419bd423a66116c18f0152846bbcca62
valderyaya <41421967+valderyaya@users.noreply.github.com>
2022-06-25 22:18:44 +0800

    Create abc257_pA.cpp

---
+++ Atcoder/abc257/abc257_pA.cpp
@@ -0,0 +1,23 @@
+#pragma GCC optimize("O2")
+#include<bits/stdc++.h>
+using namespace std;
+#define valder ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#define ll long long
+#define F first
+#define S second
+#define ALL(x) x.begin(),x.end()
+#define pii pair<int,int>
+#define REP(i,n) for(int i=0;i<n;++i)
+#define REP1(i,a,b) for(int i=a;i<=b;++i)
+#define em emplace_back
+#define lb(x) x&-x
+#define pll pair<ll,ll>
+#define mkp make_pair
+
+int n,k;
+string s;
+signed main(){
+    cin>>n>>k;
+    for(int i='A';i<='Z';++i) for(int j=0;j<n;++j) s.push_back(char(i));
+    cout<<s[k-1];
+}
