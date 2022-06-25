c359a205468bb6227a3ecf7de04d634ebb18c545
valderyaya <41421967+valderyaya@users.noreply.github.com>
2022-06-25 14:11:16 +0800

    Create abc246_pB.cpp

---
+++ Atcoder/abc246/abc246_pB.cpp
@@ -0,0 +1,22 @@
+#pragma GCC optimize("O2")
+#include<bits/stdc++.h>
+using namespace std;
+#define valder ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#define ll long long
+#define F first
+#define S second
+#define ALL(x) x.begin(),x.end()
+#define pii pair<int,int>
+#define REP(i,n) for(int i=0;i<n;++i)
+#define REP1(i,a,b) for(int i=a;i<=b;++i)
+#define em emplace_back
+#define lb(x) x&-x
+#define pll pair<ll,ll>
+#define mkp make_pair
+
+int x,y;
+signed main(){valder
+    cin>>x>>y;
+    double dx=sqrt((double)(x*x)/(x*x+y*y));
+    cout<<fixed<<setprecision(10)<<dx<<' '<<sqrt(1-dx*dx);
+}
