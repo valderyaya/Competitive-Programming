7ef32f4decb0d405e3d61e22fa8f5a5029e45158
valderyaya <41421967+valderyaya@users.noreply.github.com>
2022-06-26 22:52:16 +0800

    Create abc245_pB.cpp

---
+++ Atcoder/abc245/abc245_pB.cpp
@@ -0,0 +1,24 @@
+#pragma GCC optimize("O2")
+#include<bits/stdc++.h>
+using namespace std;
+#define valder ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#define ll long long
+#define F first
+#define S second
+#define ALL(x) x.begin(),x.end()
+#define pii pair<int,int>
+#define REP(i,n) for(int i=0;i<n;++i)
+#define REP1(i,a,b) for(int i=a;i<=b;++i)
+#define em emplace_back
+#define lb(x) x&-x
+#define pll pair<ll,ll>
+#define mkp make_pair
+
+int n,a[2005];
+signed main(){valder
+    cin>>n;
+    for(int x,i=0;i<n;++i) cin>>x,a[x]=1;
+    for(int i=0;;++i){
+        if(a[i]==0) return cout<<i,0;
+    }
+}
