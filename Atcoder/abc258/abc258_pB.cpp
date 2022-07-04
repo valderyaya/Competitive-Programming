6ecda7c0d992c49457377d80ab9f13b305780b21
valderyaya <41421967+valderyaya@users.noreply.github.com>
2022-07-04 17:36:03 +0800

    Create abc258_pB.cpp

---
+++ Atcoder/abc258/abc258_pB.cpp
@@ -0,0 +1,35 @@
+#pragma GCC optimize("O2")
+#include<bits/stdc++.h>
+using namespace std;
+#define StarBurstStream ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#define ll long long
+#define F first
+#define S second
+#define ALL(x) x.begin(),x.end()
+#define pii pair<int,int>
+#define REP(i,n) for(int i=0;i<n;++i)
+#define REP1(i,a,b) for(int i=a;i<=b;++i)
+#define em emplace_back
+#define lb(x) x&-x
+#define pll pair<ll,ll>
+#define mkp make_pair
+
+int n,dx[]={1,1,1,0,0,-1,-1,-1},dy[]={1,0,-1,1,-1,1,0,-1};
+string s[11],ans;
+string cal(int x,int y,int d){
+    string p=""; p.push_back(s[x][y]);
+    for(int i=1;i<n;++i){
+        x=(x+dx[d]+n)%n, y=(y+dy[d]+n)%n;
+        p.push_back(s[x][y]);
+    }
+    return p;
+}
+signed main(){StarBurstStream
+    cin>>n;
+    for(int i=0;i<n;++i) cin>>s[i];
+    for(int i=0;i<n;++i)
+        for(int j=0;j<n;++j)
+            for(int k=0;k<8;++k)
+                ans=max(ans,cal(i,j,k));
+    cout<<ans;
+}
