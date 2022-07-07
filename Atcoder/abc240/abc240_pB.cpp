7389b6591d2559045531c842d818ce319e78a466
valderyaya <41421967+valderyaya@users.noreply.github.com>
2022-07-07 20:08:02 +0800

    Create abc240_pB.cpp

---
+++ Atcoder/abc240/abc240_pB.cpp
@@ -0,0 +1,23 @@
+#pragma GCC optimize("O2")
+#include<bits/stdc++.h>
+using namespace std;
+#define StarBurstStream ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#define ll long long
+#define F first
+#define S second
+#define ALL(x) x.begin(),x.end()
+#define pii pair<int,int>
+#define REP(i,n) for(int i=0;i<n;++i)
+#define REP1(i,a,b) for(int i=a;i<=b;++i)
+#define em emplace_back
+#define lb(x) x&-x
+#define pll pair<ll,ll>
+#define mkp make_pair
+
+set<int> s;
+int n;
+signed main(){StarBurstStream
+    cin>>n;
+    for(int x;n--;) cin>>x,s.insert(x);
+    cout<<s.size();
+}
