2926334ecc624fd8b3af84703f6382bc2768a31a
valderyaya <41421967+valderyaya@users.noreply.github.com>
2022-07-07 20:07:48 +0800

    Create abc240_pA.cpp

---
+++ Atcoder/abc240/abc240_pA.cpp
@@ -0,0 +1,23 @@
+//#pragma GCC optimize("O2")
+#include<bits/stdc++.h>
+using namespace std;
+#define StarBurstStream ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#define ll long long
+#define F first
+#define S second
+#define ALL(x) x.begin(),x.end()
+#define pii pair<int,int>
+#define REP(i,n) for(int i=0;i<n;++i)
+#define REP1(i,a,b) for(int i=a;i<=b;++i)
+#define em emplace_back
+#define lb(x) x&-x
+#define pll pair<ll,ll>
+#define mkp make_pair
+
+int a,b;
+signed main(){StarBurstStream
+    cin>>a>>b;
+    if(a==1&&b==10||a==10&&b==1) return cout<<"Yes",0;
+    if(abs(a-b)==1) cout<<"Yes";
+    else cout<<"No";
+}
