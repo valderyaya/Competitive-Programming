88752ef4feed1c1aa230abd93248c095a8d18188
valderyaya <41421967+valderyaya@users.noreply.github.com>
2022-07-12 20:29:43 +0800

    Create abc259_pA.cpp

---
+++ Atcoder/abc259/abc259_pA.cpp
@@ -0,0 +1,24 @@
+#pragma GCC optimize("O2")
+#include<bits/stdc++.h>
+using namespace std;
+#define StarBurstStream ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#define ll long long
+#define F first
+#define S second
+#define ALL(x) x.begin(),x.end()
+#define pii pair<int,int>
+#define REP(i,n) for(int i=0;i<n;++i)
+#define REP1(i,a,b) for(int i=a;i<=b;++i)
+#define em emplace_back
+#define lb(x) x&-x
+#define pll pair<ll,ll>
+#define mkp make_pair
+
+int n,m,x,t,d;
+signed main(){StarBurstStream
+    cin>>n>>m>>x>>t>>d;
+    if(m>x) cout<<t;
+    else{
+        cout<<t-(x-m)*d;
+    }
+}
