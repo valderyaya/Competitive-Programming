3237349513c4df1b13dd43b31ed763e387a13ca5
valderyaya <41421967+valderyaya@users.noreply.github.com>
2023-01-09 21:33:35 +0800

    Create 1068.cpp

---
+++ CSES/1068.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define valder ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#define ll long long
+#define F first
+#define S second
+#define ALL(x) x.begin(),x.end()
+#define pii pair<int,int>
+#define REP(i,n) for(int i=0;i<n;++i)
+#define REP1(i,a,b) for(int i=a;i<=b;++i)
+#define em emplace_back
+#define lb(x) x&-x
+#define pll pair<ll,ll>
+ 
+ll n;
+signed main(){valder
+    cin>>n;
+    for(;n!=1;n=n&1?3*n+1:n/2) cout<<n<<' ';
+    cout<<"1\n";
+}
