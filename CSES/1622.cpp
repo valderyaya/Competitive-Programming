a6fe8efea7f2c3ad7141616208decc503b938297
valderyaya <41421967+valderyaya@users.noreply.github.com>
2023-01-09 21:35:38 +0800

    Create 1622.cpp

---
+++ CSES/1622.cpp
@@ -0,0 +1,31 @@
+//#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#include<bits/stdc++.h>
+//#include<bits/extc++.h>
+//using namespace __gnu_pbds;
+using namespace std;
+#define F first
+#define S second
+typedef long long ll;
+#define REP(i,n) for(register int i=0;i<(n);++i)
+#define REP1(i,a,b) for(register int i=(a);i<=(b);++i)
+#define em emplace_back
+#define mkp make_pair
+#define pb push_back
+#define ALL(x) (x).begin(),(x).end()
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define Aincrad ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#define lb(x) (x&-x)
+ 
+string s;
+vector<string> v;
+signed main(){Aincrad
+    cin>>s;
+    sort(ALL(s));
+    do{
+      v.em(s);
+    }while(next_permutation(ALL(s)));
+    cout<<v.size()<<"\n";
+    for(auto &i:v) cout<<i<<"\n";
+    return 0;
+}
