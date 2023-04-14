e5d7e25fc67b6dab074dc9ceb66408a3bbd85de4
valderyaya <41421967+valderyaya@users.noreply.github.com>
2023-04-14 10:28:45 +0800

    Create 1756.cpp

---
+++ CSES/1756.cpp
@@ -0,0 +1,27 @@
+//#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+#include<bits/stdc++.h>
+//#include<bits/extc++.h>
+//using namespace __gnu_pbds;
+using namespace std;
+#define F first
+#define S second
+#define REP(i,n) for(int i=0;i<(n);++i)
+#define REP1(i,a,b) for(int i=(a);i<=(b);++i)
+#define em emplace_back
+#define mkp make_pair
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define Rosario ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+//#define lb(x) (x&-x)
+
+
+signed main(){Rosario
+    int n,m;
+    cin>>n>>m;
+    for(int x,y;m--;){
+        cin>>x>>y;
+        if(x>y) swap(x,y);
+        cout<<x<<' '<<y<<"\n";
+    }
+    return 0;
+}
