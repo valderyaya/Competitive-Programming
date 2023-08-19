0b1bbcd06d72a319d79a6847131bde80b69a6835
valderyaya <41421967+valderyaya@users.noreply.github.com>
2023-08-19 15:05:10 +0800

    Update 2031.cpp

--- TIOJ/2031.cpp
+++ TIOJ/2031.cpp
@@ -1,7 +1,5 @@
 #pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
 #include<bits/stdc++.h>
-//#include<bits/extc++.h>
-//using namespace __gnu_pbds;
 using namespace std;
 typedef long long ll;
 #define F first
@@ -14,8 +12,6 @@ typedef long long ll;
 #define pii pair<int,int>
 #define pll pair<ll,ll>
 #define Rosario ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
-//#define lb(x) (x&-x)
-#define wait system("pause");
 
 inline void read(int &ret){
     ret=0;char c=getchar_unlocked();
@@ -24,11 +20,11 @@ inline void read(int &ret){
 }
 
 const int maxn=500001;
-int tri[20*maxn][2],ed[20*maxn],cnt[20*maxn],rt;
+int tri[2][3*maxn],ed[3*maxn],cnt[3*maxn],rt;
 
 inline int node(){
     ++rt;
-    tri[rt][0]=tri[rt][1]=ed[rt]=cnt[rt]=0;
+    tri[0][rt]=tri[1][rt]=ed[rt]=cnt[rt]=0;
     return rt;
 }
 
@@ -36,9 +32,9 @@ inline void insert(int x,int id){
     int u=0,c;
     for(int i=19;~i;--i){
         c=((x>>i)&1);
-        if(!tri[u][c]) tri[u][c]=node();
-        u=tri[u][c];
-        cnt[u]++;
+        if(!tri[c][u]) tri[c][u]=node();
+        u=tri[c][u];
+        ++cnt[u];
     }
     ed[u]=id;
 }
@@ -47,35 +43,37 @@ inline int find(int x,int sum){
     int u=0,c;
     for(int i=19;~i;--i){
         c=((x>>i)&1)^1;
-        if(tri[u][c]&&cnt[tri[u][c]]>=sum) u=tri[u][c];
+        if(tri[c][u]&&cnt[tri[c][u]]>=sum) u=tri[c][u];
         else {
-            if(tri[u][c]) sum-=cnt[tri[u][c]];
-            u=tri[u][c^1];
+            if(tri[c][u]) sum-=cnt[tri[c][u]];
+            u=tri[c^1][u];
         }
     }
     return ed[u];
 }
 
-int n,m,low[maxn],dfn[maxn],col[maxn],w[maxn],t=1,cl=0,star,r[maxn];
-ll W[maxn];
-vector<int> v1[maxn],v2[maxn];
-stack<int> st;
+int n,m,low[maxn],dfn[maxn],col[maxn],w[maxn],t,cl,star,r[maxn];
+ll W[maxn],mx;
+vector<int> v1[maxn],v2[maxn],st;
 bitset<maxn> in;
 
 void dfs(int x){
-    low[x]=dfn[x]=t++;st.emplace(x);in[x]=1;
+    low[x]=dfn[x]=++t;st.em(x);in[x]=1;
     for(auto &i:v1[x])
         if(!dfn[i]) dfs(i),low[x]=min(low[x],low[i]);
         else if(in[i]&&dfn[i]<dfn[x]) low[x]=min(low[x],dfn[i]);
     if(dfn[x]==low[x]){++cl;
-        for(int u=0;u!=x;st.pop()) u=st.top(),in[u]=0,col[u]=cl,W[cl]+=w[u];
+        for(int u=0;u!=x;st.pop_back()) u=st.back(),in[u]=0,col[u]=cl,W[cl]+=w[u];
     }
 }
 
-int main(){Rosario
+void dfs2(int x, ll val){
+    mx=max(mx, val);
+    for(auto &i:v2[x]) dfs2(i, val+W[i]);
+}
+
+int main(){
     read(n),read(star);
-    rt=-1;
-    node();
     REP1(i,1,n) read(w[i]);
     REP1(i,1,n){
         read(m),read(r[i]);
@@ -96,16 +94,7 @@ int main(){Rosario
     }
     dfs(star);
     REP1(i,1,n) for(auto &j:v1[i]) if(col[i]!=col[j]) v2[col[i]].em(col[j]);
-    queue<pll> q;
-    q.emplace(mkp((ll)col[star],W[col[star]]));
-    ll mx=0;
-    pll x;
-    while(!q.empty()){
-        x=q.front();q.pop();
-        mx=max(mx,x.S);
-        for(auto &i:v2[x.F]) q.emplace(mkp(i,x.S+W[i]));
-    }
-    cout<<mx<<"\n";
-//    system("pause");
+    dfs2(col[star], W[col[star]]);
+    printf("%lld\n",mx);
     return 0;
 }
