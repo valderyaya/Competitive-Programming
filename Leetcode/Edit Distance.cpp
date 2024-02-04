942118e2d51b4209c6f3b570b538583b8bacb0cc
valderyaya <41421967+valderyaya@users.noreply.github.com>
2024-02-04 16:58:46 +0800

    Create Edit Distance.cpp

---
+++ Leetcode/Edit Distance.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int dp[501][501];
+        for(int i=0;i<word1.size()+1;++i)
+            for(int j=0;j<word2.size()+1;++j){
+                if(i==0) dp[i][j]=j;
+                else if(j==0) dp[i][j]=i;
+                else if(word1[i-1]==word2[j-1]) dp[i][j]=dp[i-1][j-1];
+                else dp[i][j]=1+min({dp[i][j-1],dp[i-1][j-1],dp[i-1][j]});
+            }
+        return dp[word1.size()][word2.size()];
+    }
+};
