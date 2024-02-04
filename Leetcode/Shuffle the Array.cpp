cef8635834b0f913b7fdcf09ea446aef2844e41f
valderyaya <41421967+valderyaya@users.noreply.github.com>
2024-02-04 17:04:48 +0800

    Create Shuffle the Array.cpp

---
+++ Leetcode/Shuffle the Array.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> v;
+        for(int i=0;i<n;++i) v.emplace_back(nums[i]),v.emplace_back(nums[i+n]);
+        return v;
+    }
+};
